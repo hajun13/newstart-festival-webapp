64b00591daf1866a3063ccde496143710209268f
hajun13 <chhajun01@naver.com>
2026-05-09 22:33:17 +0900

    chore: update layout metadata and root structure configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/app/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NEWSTART 생명의 열쇠",
+  title: "2026 서중한합회 청소년 페스티벌",
   description: "2026 서중한합회 청소년 페스티벌 운영 웹앱"
 };
 
